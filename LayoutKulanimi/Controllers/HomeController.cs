f8859c997674a22a3246a734d6d604c7e2b59e51
Aydoğan Selçuk Kılıçtaş <92909567+Sphinxs77@users.noreply.github.com>
2021-12-22 14:27:02 +0300

    Add project files.

---
+++ LayoutKulanimi/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using LayoutKullanimi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace LayoutKullanimi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+
+        {
+            var model = new List<Urun>()
+            {
+               new Urun
+               {
+                   Id=1,
+                   UrunAdi="chai",
+                   Fiyat=5
+               },
+
+               new Urun
+               {
+                   Id=2,
+                   UrunAdi="elma",
+                   Fiyat=7
+               }
+            };
+            ViewBag.Title = "Ana Sayfa";
+            return View(model);
+        }
+    }
+}
